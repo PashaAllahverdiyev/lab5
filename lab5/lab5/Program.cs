f9fecc64bc3755344af954b549cc7c259b6691be
PashaAllahverdiyev <pashama@code.edu.az>
2024-03-07 10:50:42 +0400

    first commit

---
+++ lab5/lab5/Program.cs
@@ -0,0 +1,26 @@
+﻿//int a = 5;
+//for (int i = 1; i <= a; i++)
+//{
+//    for (int j = 1; j <= i; j++)
+//    {
+//        Console.Write("*");
+//    }
+//    Console.WriteLine();
+//}
+int[] firstArr = { 211, 9, -6, 12, 34, 56, 32, 5, 19 };
+int[] secondArr = { 19, -6, 32 };
+bool check = true;
+for (int i = 0; i < firstArr.Length; i++)
+{
+  for (int j = 0; j < secondArr.Length; j++)
+    {
+        if (firstArr[i]==secondArr[j])
+        {
+            Console.WriteLine(check=true);
+        }
+        else
+        {
+            Console.WriteLine(check &= false);
+        }
+    }
+}
