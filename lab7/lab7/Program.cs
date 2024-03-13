89ae9480e7c25fe23d2190f2490e2f7db8ed2988
PashaAllahverdiyev <129064604+PashaAllahverdiyev@users.noreply.github.com>
2024-03-14 00:33:34 +0400

    Add files via upload

---
+++ lab7/lab7/Program.cs
@@ -0,0 +1,18 @@
+﻿input:
+Console.Write("Say daxil edin: ");
+bool check = int.TryParse(Console.ReadLine(), out int val);
+if (check)
+{
+    goto input;
+}
+int[] arr = new int[val];
+arr[0] = 0;
+arr[1] = 1;
+for (int i = 2; i < arr.Length; i++)
+{
+    arr[i] = arr[i - 2] + arr[i - 1];
+}
+foreach (int i in arr)
+{
+    Console.WriteLine(i);
+}
